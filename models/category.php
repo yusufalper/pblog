c0a836ec72839e8f862d1330f15e13731d8a98fe
yusufalper <yusufalpersari@yandex.com>
2018-07-17 17:35:42 +0300

    category order fix

--- models/category.php
+++ models/category.php
@@ -3,14 +3,14 @@ class CategoryModel extends Model
 {
     public function Index()
     {
-        $this->query("SELECT * FROM categories ORDER BY id");
+        $this->query("SELECT * FROM categories ORDER BY id DESC");
         $rows = $this->resultSet();
         return $rows;
     }
 
     public function selected()
     {
-        $this->query("SELECT * FROM posts WHERE category_id=:cid");
+        $this->query("SELECT * FROM posts WHERE category_id=:cid ORDER BY id DESC");
         $this->bind(':cid', $_POST['cid']);
         $rows = $this->resultSet();
         return $rows;
