8b58f63e635e582ad389c538ea9319bcdf4b1f73
yusufalper <yusufalpersari@yandex.com>
2018-07-24 15:04:59 +0300

    pblog.sql is updated

--- pblog.sql
+++ pblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 20, 2018 at 11:47 AM
+-- Generation Time: Jul 24, 2018 at 03:03 PM
 -- Server version: 5.7.22-0ubuntu18.04.1
 -- PHP Version: 7.2.7-0ubuntu0.18.04.2
 
@@ -27,18 +27,41 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_turkish_ci NOT NULL
+  `category_id` int(11) NOT NULL,
+  `cat_name` varchar(255) COLLATE utf8_turkish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `name`) VALUES
+INSERT INTO `categories` (`category_id`, `cat_name`) VALUES
 (1, 'Others'),
 (2, 'Technology'),
 (3, 'Software Development');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `comment` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `comment_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `user_id`, `post_id`, `comment`, `comment_time`) VALUES
+(1, 15, 13, 'That is a comment!!', '2018-07-20 15:04:54'),
+(2, 15, 14, 'dsadasdsadasdsadsa', '2018-07-20 15:34:01'),
+(3, 16, 13, 'dsdasdsadsa', '2018-07-20 15:34:15');
 
 -- --------------------------------------------------------
 
@@ -47,7 +70,7 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
   `category_id` int(11) NOT NULL DEFAULT '1',
@@ -59,6 +82,14 @@ CREATE TABLE `posts` (
   `status` tinyint(1) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `user_id`, `title`, `category_id`, `tags`, `description`, `content`, `source_link`, `post_date`, `status`) VALUES
+(13, 15, 'AAAA', 1, 'aaa', 'aaa', 'Lorem Ipsum', 'aaa', '2018-07-20 14:40:01', 1),
+(14, 16, 'bbb', 2, 'bbb', 'bbb', 'Lorem Ipsum', 'bbb', '2018-07-20 14:40:35', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -66,7 +97,7 @@ CREATE TABLE `posts` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `email` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
   `name` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
   `surname` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
@@ -77,6 +108,17 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `name`, `surname`, `password`, `phone`, `bio`, `register_date`) VALUES
+(15, 'a@a.com', 'aaa', 'aaa', '47bce5c74f589f4867dbd57e9ca9f808', '77-777-777-7777', NULL, '2018-07-20 14:39:36'),
+(16, 'b@b.com', 'bbb', 'bbb', '08f8e0260c64418510cefb2b06eee5cd', '88-888-888-8888', NULL, '2018-07-20 14:40:16'),
+(17, 'dsadassda@dsads.comddsads', 'zzz', 'dasdsadsa', '5aa21df4a324cd1cfc0755c65e410695', '99-999-999-9999', NULL, '2018-07-23 12:06:18'),
+(18, 'zzz@z.com', 'zzzzzzzz', 'zzzzzzz', 'a661465fb2fdad3509478a1a869c1d52', '99-999-999-9999', NULL, '2018-07-23 12:08:14'),
+(19, 'q@q.qom', 'qqq', 'qqq', 'b2ca678b4c936f905fb82f2733f5297f', '77-777-777-7777', NULL, '2018-07-23 12:08:47');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -84,13 +126,21 @@ CREATE TABLE `users` (
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`post_id`),
   ADD KEY `id` (`user_id`),
   ADD KEY `category_id` (`category_id`);
 
@@ -98,7 +148,7 @@ ALTER TABLE `posts`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -108,27 +158,39 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
