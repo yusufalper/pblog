6d8c01b44a3b53b2dc4ee389535c624e5335271d
yusufalper <yusufalpersari@yandex.com>
2018-08-02 16:19:09 +0300

    fix -categories

--- views/Categories/index.php
+++ views/Categories/index.php
@@ -1,12 +1,10 @@
 <h2>Categories</h2>
 <div class="div-categories">
-	<div >
-		<?php foreach ($viewModel as $item): ?>
-			<form method="POST" action="<?php echo ROOT_PATH; ?>categories/selected">
-				<input type="hidden" name="cid" class="form-control" value="<?php echo $item['category_id'] ?>" />
-				<input class="btn btn-danger category-button" name="submit" type="submit" value= "<?php echo $item['cat_name']; ?>"/>
-			</form>
+	<?php foreach ($viewModel as $item): ?>
+	<form method="POST" action="<?php echo ROOT_PATH; ?>categories/selected">
+		<input type="hidden" name="cid" class="form-control" value="<?php echo $item['category_id'] ?>" />
+		<input class="btn btn-danger category-button" name="submit" type="submit" value="<?php echo $item['cat_name']; ?>" />
+	</form>
 
-		<?php endforeach;?>
-	</div>
+	<?php endforeach;?>
 </div>
